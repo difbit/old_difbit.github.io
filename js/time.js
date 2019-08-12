e92dde61ef1ece9ab277db03f7ac4741f4137d75
difbit <fruktoosijee@gmail.com>
2019-08-12 21:50:53 +0300

    count()

--- js/time.js
+++ js/time.js
@@ -1,9 +1,18 @@
 var d = new Date();
 var s = 0;
 var t;
+var timer_is_on = 0;
+
 function time()
 {
 	document.getElementById('time').value = s;
 	s = s + 1;
 	t = setTimeout(time, 1000);
+}
+
+function count() {
+	if (!timer_is_on) {
+		timer_is_on = 1;
+		time();
+	}
 }
